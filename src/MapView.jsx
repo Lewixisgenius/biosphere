dd76b32eb34d74b2d5c05a30298680775c98d228
Lewix <lewixisgenius@gmail.com>
2025-07-23 14:58:41 +0300

    Update MapView.jsx

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -9,18 +9,6 @@ export default function App() {
   const [loadingSummary, setLoadingSummary] = useState(false);
 
 
-  import L from "leaflet";
-
-// Public klasöründen ikonları ayarla
-const customIcon = new L.Icon({
-  iconUrl: "/marker-icon.png",
-  shadowUrl: "/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-
   useEffect(() => {
     const fetchAll = async () => {
       try {
